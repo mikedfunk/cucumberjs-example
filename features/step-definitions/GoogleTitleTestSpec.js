36906ed6c2086b8b08bced81ec80b423ff2d42bb
Mike Funk <mike@mikefunk.com>
2015-07-09 09:48:09 -0700

    fixed problem with browser trying to search before the page is loaded

--- features/step-definitions/GoogleTitleTestSpec.js
+++ features/step-definitions/GoogleTitleTestSpec.js
@@ -7,9 +7,10 @@ module.exports = function () {
 
   this.When(/^I visit google\.com$/, function (next) {
     // visit a page (baseUrl is set in wdio.conf.js)
-    browser.url('/');
+    browser.url('/').
+
     // tell cucumber this test passes
-    next();
+    call(next);
   });
 
   this.Then(/^I should see Google in the title$/, function (next) {
@@ -17,11 +18,14 @@ module.exports = function () {
     // get the page content
     var _this = this;
     browser.getText('body').
+
       // this is a promise so we can do .then()
       then(function(value) {
+
         // use chai to check that the content contains Google. Will throw an
         // exception and be considered a failure on the cli if it fails.
         _this.expect(value).to.match(/Google/);
+
         // next() doesn't work within the promise for some reason but this does
       }).call(next);
   });
